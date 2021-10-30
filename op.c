06044dac3cc0797aa9b9d5ebba4c9e2011a9714c
GOPAL181 <92174457+GOPAL181@users.noreply.github.com>
2021-10-29 22:54:56 -0600

    Add files via upload
    write a program that take value from the user and multiply it by 31,32,33using only bitwise operator

---
+++ op.c
@@ -0,0 +1,16 @@
+ /* write a program that take value from the user and multiply it by 31,32,33using only bitwise operator*/
+#include<stdio.h>
+int main(){
+    int a,b,c,d;
+    printf("enter the value");
+    scanf("%d",&a);
+    b=a<<5;
+    c=b-a;
+    d=b+a;
+    printf("value is after multiplying by 31 is=");
+    printf("%d",c);
+    printf("\nvalue is after multiplying by 32 is=");
+    printf("%d",b);
+    printf("\nvalue is after multiplying by 33 is=");
+    printf("%d",d);
+}
