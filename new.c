a98eb7ef545ac26bdc6264be64722a412703bc24
Gopal Sharma <92174457+GOPAL181@users.noreply.github.com>
2021-11-15 22:37:56 +0530

    Add files via upload
    write a program which print the following pattern
    *
    **
    ***
    ****
    ***
    **
    *

--- new.c
+++ new.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
     int i=0, j=0,k=1,l=1;
-    for(i=0;i<=5;i++){
+    for(i=0;i<=6;i++){
         for(j=0;j<=i;j++){
-            if(i==0||i==1||i==3||i==2||j==0||i==1&&j==1||i==2&&j==2||i==4&&j==1){
+            if(i==0||i==1||i==3||i==2||j==0||i==1&&j==1||i==2&&j==2||i==4&&j<=2||i==5&&j<=1){
                 printf("*");
             }else{
                 printf(" ");
