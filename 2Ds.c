33c8cbaa622eed2f5c187591749d8b3a3b7f4fe3
Gopal Sharma <92174457+GOPAL181@users.noreply.github.com>
2021-12-24 19:02:25 +0530

    Add files via upload
    write a program to sum the 2D array in row wise

---
+++ 2Ds.c
@@ -0,0 +1,20 @@
+#include<stdio.h>//sum 2D array row wise
+void main(){
+    int i,j,m,n,sum=0;
+printf("enter the val");
+scanf("%d%d",&n,&m);
+    int a[n][m];
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            printf("enter the value");
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            sum+=a[i][j];
+        }
+        printf("%d\n",sum);
+        sum=0;
+    }
+}
