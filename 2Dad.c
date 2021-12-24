d9e6eb05062c8d705b90d38f0c9ff0042d2e06b1
Gopal Sharma <92174457+GOPAL181@users.noreply.github.com>
2021-12-24 19:33:30 +0530

    Add files via upload
    write a program which take value from user in matrix and print its anti diagonal

---
+++ 2Dad.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>//sum 2D array row wise
+void main(){
+    int i,j,m,n,sum=0;
+printf("enter the val");
+scanf("%d%d",&n,&m);
+    int a[n][m];
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            printf("enter the value");
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            
+        if((i+j)==m-1){
+            printf("%d",a[i][j]);
+        }  
+        else{
+            printf(" ");}
+        }
+        printf("\n");
+    }    
+}
