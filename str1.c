354c7724719464bb0deac5a8c5522ad00e097076
Gopal Sharma <92174457+GOPAL181@users.noreply.github.com>
2021-12-31 17:55:38 +0530

    Add files via upload
    write a program which take string  from user and print like given below without using loop.
    s
    sa
    sac
    sach
    sachi
    sachin

---
+++ str1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+    char str[100];
+    int i,j,len1;
+    scanf("%[^\n]s",str);
+    len1=strlen(str);
+    for(i=1;i<=len1;i++){
+        printf("%*.s\n",i,str);
+    }
+} 
